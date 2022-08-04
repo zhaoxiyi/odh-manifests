17d5e9084b4e013c12f5340ba269327d74bbf4b5
Chad Roberts <croberts@redhat.com>
2022-08-04 13:39:43 -0400

    Add overlay for model-mesh serving component (#599)

--- tests/basictests/modelmesh.sh
+++ tests/basictests/modelmesh.sh
@@ -7,6 +7,9 @@ MY_DIR=$(readlink -f `dirname "${BASH_SOURCE[0]}"`)
 source ${MY_DIR}/../util
 RESOURCEDIR="${MY_DIR}/../resources"
 
+MODEL_PROJECT="${ODHPROJECT}-model"
+PREDICTOR_NAME="example-mnist-predictor"
+
 
 os::test::junit::declare_suite_start "$MY_SCRIPT"
 
@@ -18,34 +21,38 @@ function check_resources() {
     os::cmd::try_until_text "oc get service modelmesh-serving" "modelmesh-serving" $odhdefaulttimeout $odhdefaultinterval
 }
 
-function setup_test_serving() {
-    header "Setting up test modelmesh serving"
+function setup_test_serving_namespace() {
+    oc new-project ${MODEL_PROJECT}
+    header "Setting up test modelmesh serving in ${MODEL_PROJECT}"
     SECRETKEY=$(openssl rand -hex 32)
     sed -i "s/<secretkey>/$SECRETKEY/g" ${RESOURCEDIR}/modelmesh/sample-minio.yaml
-    os::cmd::expect_success "oc apply -f ${RESOURCEDIR}/modelmesh/sample-minio.yaml"
-    os::cmd::try_until_text "oc get pods -l app=minio --field-selector='status.phase=Running' -o jsonpath='{$.items[*].metadata.name}' | wc -w" "1" $odhdefaulttimeout $odhdefaultinterval
-    os::cmd::expect_success "oc apply -f ${RESOURCEDIR}/modelmesh/mnist-sklearn.yaml"
-    os::cmd::try_until_text "oc get pods -l name=modelmesh-serving-mlserver-0.x --field-selector='status.phase=Running' -o jsonpath='{$.items[*].metadata.name}' | wc -w" "2" $odhdefaulttimeout $odhdefaultinterval
-    os::cmd::try_until_text "oc get predictor example-mnist-predictor -o jsonpath='{$.status.activeModelState}'" "Loaded" $odhdefaulttimeout $odhdefaultinterval
+    os::cmd::expect_success "oc apply -f ${RESOURCEDIR}/modelmesh/sample-minio.yaml -n ${MODEL_PROJECT}"
+    os::cmd::try_until_text "oc get pods -n ${MODEL_PROJECT} -l app=minio --field-selector='status.phase=Running' -o jsonpath='{$.items[*].metadata.name}' | wc -w" "1" $odhdefaulttimeout $odhdefaultinterval
+    os::cmd::expect_success "oc apply -f ${RESOURCEDIR}/modelmesh/mnist-sklearn.yaml -n ${MODEL_PROJECT}"
+    os::cmd::try_until_text "oc get pods -n ${ODHPROJECT} -l app=odh-model-controller --field-selector='status.phase=Running' -o jsonpath='{$.items[*].metadata.name}' | wc -w" "3" $odhdefaulttimeout $odhdefaultinterval
+    os::cmd::try_until_text "oc get pods -n ${MODEL_PROJECT} -l name=modelmesh-serving-mlserver-0.x --field-selector='status.phase=Running' -o jsonpath='{$.items[*].metadata.name}' | wc -w" "2" $odhdefaulttimeout $odhdefaultinterval
+    os::cmd::try_until_text "oc get predictor -n ${MODEL_PROJECT} example-mnist-predictor -o jsonpath='{$.status.activeModelState}'" "Loaded" $odhdefaulttimeout $odhdefaultinterval
+    oc project ${ODHPROJECT}
 }
 
 function teardown_test_serving() {
     header "Tearing down test modelmesh serving"
+    oc project ${MODEL_PROJECT}
     os::cmd::expect_success "oc delete -f ${RESOURCEDIR}/modelmesh/sample-minio.yaml"
     os::cmd::try_until_text "oc get pods -l app=minio --field-selector='status.phase=Running' -o jsonpath='{$.items[*].metadata.name}' | wc -w" "0" $odhdefaulttimeout $odhdefaultinterval
     os::cmd::expect_success "oc delete -f ${RESOURCEDIR}/modelmesh/mnist-sklearn.yaml"
     os::cmd::try_until_text "oc get pods -l name=modelmesh-serving-mlserver-0.x --field-selector='status.phase=Running' -o jsonpath='{$.items[*].metadata.name}' | wc -w" "0" $odhdefaulttimeout $odhdefaultinterval
+    os::cmd::expect_success "oc delete project ${MODEL_PROJECT}"
+    oc project ${ODHPROJECT}
 }
 
 function test_inferences() {
     header "Testing inference from example mnist model"
-    oc port-forward --address 0.0.0.0 service/modelmesh-serving  8008 -n ${ODHPROJECT} &
-    #capture pid of port forward
-    pf_pid=$!
+    oc project ${MODEL_PROJECT}
+    route=$(oc get route ${PREDICTOR_NAME} --template={{.spec.host}})
     # The result of the inference call should give us back an "8" for the data part of the output
-    os::cmd::try_until_text "curl -X POST -k http://localhost:8008/v2/models/example-mnist-predictor/infer -d '{\"inputs\": [{ \"name\": \"predict\", \"shape\": [1, 64], \"datatype\": \"FP32\", \"data\": [0.0, 0.0, 1.0, 11.0, 14.0, 15.0, 3.0, 0.0, 0.0, 1.0, 13.0, 16.0, 12.0, 16.0, 8.0, 0.0, 0.0, 8.0, 16.0, 4.0, 6.0, 16.0, 5.0, 0.0, 0.0, 5.0, 15.0, 11.0, 13.0, 14.0, 0.0, 0.0, 0.0, 0.0, 2.0, 12.0, 16.0, 13.0, 0.0, 0.0, 0.0, 0.0, 0.0, 13.0, 16.0, 16.0, 6.0, 0.0, 0.0, 0.0, 0.0, 16.0, 16.0, 16.0, 7.0, 0.0, 0.0, 0.0, 0.0, 11.0, 13.0, 12.0, 1.0, 0.0]}]}' | jq '.outputs[0].data[0]'" "8" $odhdefaulttimeout $odhdefaultinterval
-    #kill off port foward process
-    kill -9 $pf_pid
+    os::cmd::try_until_text "curl -X POST -k ${route}/infer -d '{\"inputs\": [{ \"name\": \"predict\", \"shape\": [1, 64], \"datatype\": \"FP32\", \"data\": [0.0, 0.0, 1.0, 11.0, 14.0, 15.0, 3.0, 0.0, 0.0, 1.0, 13.0, 16.0, 12.0, 16.0, 8.0, 0.0, 0.0, 8.0, 16.0, 4.0, 6.0, 16.0, 5.0, 0.0, 0.0, 5.0, 15.0, 11.0, 13.0, 14.0, 0.0, 0.0, 0.0, 0.0, 2.0, 12.0, 16.0, 13.0, 0.0, 0.0, 0.0, 0.0, 0.0, 13.0, 16.0, 16.0, 6.0, 0.0, 0.0, 0.0, 0.0, 16.0, 16.0, 16.0, 7.0, 0.0, 0.0, 0.0, 0.0, 11.0, 13.0, 12.0, 1.0, 0.0]}]}' | jq '.outputs[0].data[0]'" "8" $odhdefaulttimeout $odhdefaultinterval
+    oc project ${ODHPROJECT}
 }
 
 function setup_monitoring() {
@@ -55,13 +62,13 @@ function setup_monitoring() {
 
 function test_metrics() {
     header "Checking metrics for total models loaded, should be 1 since we have 1 model being served"   
-    monitoring_token=`oc sa get-token prometheus-k8s -n openshift-monitoring`
+    monitoring_token=$(oc sa get-token prometheus-k8s -n openshift-monitoring)
     os::cmd::try_until_text "oc -n openshift-monitoring exec -c prometheus prometheus-k8s-0 -- curl -k -H \"Authorization: Bearer $monitoring_token\" 'https://thanos-querier.openshift-monitoring.svc:9091/api/v1/query?query=modelmesh_models_loaded_total' | jq '.data.result[0].value[1]'" "1" $odhdefaulttimeout $odhdefaultinterval
 }
 
 setup_monitoring
 check_resources
-setup_test_serving
+setup_test_serving_namespace
 test_inferences
 test_metrics
 teardown_test_serving
